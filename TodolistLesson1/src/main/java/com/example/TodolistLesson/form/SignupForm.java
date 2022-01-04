ea5e43638a12ecf23756194a6a8654fb6d0621a1
yamano <hikaru32699@gmail.com>
2022-01-04 15:18:17 +0900

    issue#2 サインアップ時記号が使えない不具合の修正

--- TodolistLesson1/src/main/java/com/example/TodolistLesson/form/SignupForm.java
+++ TodolistLesson1/src/main/java/com/example/TodolistLesson/form/SignupForm.java
@@ -19,7 +19,7 @@ public class SignupForm {
 	
 	@NotBlank(groups = ValidGroup1.class)
 	@Length(min = 8, max = 100, groups = ValidGroup2.class)
-	@Pattern(regexp = "^[a-zA-Z0-9]+$", groups = ValidGroup2.class)
+	@Pattern(regexp = "^[a-zA-Z0-9 -/:-@\\[-\\`\\{-\\~]+$", groups = ValidGroup2.class)
 	private String password;
 	
 	@NotBlank(groups = ValidGroup1.class)
