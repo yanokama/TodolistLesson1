f60d0fc4ff2a9e34b668302c0d383a684a57d579
yamano <hikaru32699@gmail.com>
2022-08-21 16:47:06 +0900

    タイトル追加時の画面遷移を無くす（作業中）
    追加処理分のみ。更新と削除は次回。
    今動かすとdeleteで４０４かつ、delete側のtdタグ内に古いIdが残っている

--- TodolistLesson1/src/main/resources/static/js/todo/title.js
+++ TodolistLesson1/src/main/resources/static/js/todo/title.js
@@ -2,6 +2,41 @@
 
 /**画面ロード時の処理 */
 jQuery(function($){
+    var token = $('meta[name="_csrf"]').attr("content");
+    var header = $('meta[name="_csrf_header"]').attr("content");
+    $(document).ajaxSend(function(e, xhr, options) {
+      xhr.setRequestHeader(header, token);
+    });	
+    
+	/**追加ボタンを押したときの処理 */
+	$('#btn-create').on('click',function(){
+        ///btnのname属性値とJava側のメソッドが対応する
+	    const param = $(this).attr('name')		
+		$.ajax({
+		      type: 'PUT',
+		      dataType: 'json',
+		      url: '/todo/title',
+		      data: param
+		    }).done(function(response, textStatus, jqXHR ) {
+				///引数の意味確認と必要に応じて直す（少なくともtitleJsonに置き換えは必要）
+				alert("OK.");///テスト用
+				let titleJson = response.titles[0]	//必ず1番目の要素に格納される。他は空き。
+				addLine(titleJson);					
+		    }).fail(function(jqXHR, textStatus, errorThrown) {
+				alert("Failed.");
+		});
+	});
+	
+	/**新しい行の追加 */
+	function addLine(titleJson){
+        let clone = $('#titles tr:first').clone(true);
+        clone.find('.list-name').val(titleJson.listName);        
+        const url = '/todo/todo/' + titleJson.listId;
+        clone.find('.list-id').attr('href', url);
+        ///TODO：この際delete側のlistIdも更新する  
+        $('#titles').append(clone[0]);
+	}		
+
 	/**削除ボタンを押したときの処理 */
 	$('.title-delete-btn').on('click',function(){
 		if (confirm('タイトルを削除します。よろしいですか？')) {
